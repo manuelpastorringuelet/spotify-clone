23ddf1135c206dd05af8163020de946aab94a319
Manuel Pastor <pastorringuelet@gmail.com>
2023-06-20 19:36:15 +0200

    implemented SearchInput

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,7 +1,34 @@
 "use client";
 
+import qs from "query-string";
+
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Input from "./Input";
+
 const SearchInput = () => {
-  return <div>Seach Input!</div>;
+  const router = useRouter();
+  const [value, setValue] = useState("");
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  useEffect(() => {
+    const query = { title: debouncedValue };
+    const url = qs.stringifyUrl({
+      url: "/search",
+      query: query,
+    });
+
+    router.push(url);
+  }, [debouncedValue, router]);
+
+  return (
+    <Input
+      placeholder="What do you want to listen to?"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
 };
 
 export default SearchInput;
--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -10,3 +10,5 @@ function useDebounce<T>(value: T, delay?: number): T {
 
   return debouncedValue;
 }
+
+export default useDebounce
