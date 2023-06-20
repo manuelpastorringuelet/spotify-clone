c344ac8ae832561c118b87d571579184f9c8d1ae
Manuel Pastor <pastorringuelet@gmail.com>
2023-06-20 20:04:53 +0200

    implemented handleLike

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -4,6 +4,7 @@ import { useSessionContext } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { toast } from "react-hot-toast";
 
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
@@ -41,8 +42,40 @@ const LikeButton: React.FC<LikeButtonProps> = ({ songId }) => {
 
   const Icon = isLiked ? AiFillHeart : AiOutlineHeart;
 
+  const handleLike = async () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    if (isLiked) {
+      const { error } = await supabaseClient
+        .from("liked_songs")
+        .delete()
+        .eq("user_id", user.id)
+        .eq("song_id", songId);
+      if (error) {
+        toast.error(error.message);
+      } else {
+        setIsLiked(false);
+      }
+    } else {
+      const { error } = await supabaseClient
+        .from("liked_songs")
+        .insert([{ user_id: user.id, song_id: songId }]);
+
+      if (error) {
+        toast.error(error.message);
+      } else {
+        setIsLiked(true);
+        toast.success("Song liked!");
+      }
+    }
+
+    router.refresh();
+  };
+
   return (
-    <button>
+    <button onClick={handleLike} className="hover:opacity-75 transition">
       <Icon color={isLiked ? "#22c55e " : "white"} size={25} />
     </button>
   );
