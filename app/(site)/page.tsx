6ab0dd1522ec20204e5de65bc78551c7f5c2b900
Manuel Pastor <pastorringuelet@gmail.com>
2023-06-26 20:14:43 +0200

    fixed SUPABASE_SERVICE_ROLE_KEY

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   const songs = await getSong();
 
   return (
-    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-hidden">
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
         <div className="mb-2">
           <h1
